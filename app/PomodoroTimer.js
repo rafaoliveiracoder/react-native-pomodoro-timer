0e560939e86b6fe1d8e4bbf9205aabe8970b980d
Rafael Oliveira <rafaoliveiracoder@gmail.com>
2021-01-21 00:11:30 -0300

    Main structure created.

---
+++ app/PomodoroTimer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+import HomeScreen from './screens/HomeScreen'
+
+const PomodoroTimer = () => {
+  return (
+    <View>
+      <HomeScreen />
+    </View>
+  );
+}
+
+export default PomodoroTimer;
+
