ebdd56339a126e81af9b33ed56d3c9eb7d2e09de
Rafael Oliveira <rafaoliveiracoder@gmail.com>
2021-01-22 18:02:51 -0300

    Redux clock implementation.

---
+++ app/api/helper.js
@@ -0,0 +1,19 @@
+
+  export class Timer {
+    constructor(func){
+        this.timer = null;
+        this.func = func;
+    }
+  
+    start() {
+      if(!this.timer ){
+          this.timer = setInterval(this.func, 1000);
+      }
+    }
+  
+    stop() {
+      if(this.timer){
+        clearInterval(this.timer);
+      }
+    }
+  }
---
+++ app/utils/Timer.js
@@ -0,0 +1,33 @@
+export default class Timer{
+    constructor(){
+        this.timer = null;
+        this.func = null;
+        this.dispatch = null;
+        this.is_running = false;
+    }
+
+    setDispatcher(dispatch, func){
+        this.func = func;
+        this.dispatch = dispatch;
+    }
+
+    start(){
+        if(!this.timer && this.dispatch && this.func){
+            //Updates redux timer state from here
+            this.timer= setInterval(()=>this.dispatch(this.func()), 1000);
+            this.is_running = true;
+        }else{
+            clearInterval(this.timer);
+        }
+    }
+    stop(){
+        if(this.timer){
+            clearInterval(this.timer);
+            this.is_running = false;
+        }
+    }
+
+    get isRunning(){
+        return this.is_running;
+    }
+}
