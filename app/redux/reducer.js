dd88e3e1a050d3ceb92a93ed5bad68332f50a279
Rafael Oliveira <rafaoliveiracoder@gmail.com>
2021-01-25 15:10:39 -0300

    Actions with switch clauses.

--- app/redux/reducer.js
+++ app/redux/reducer.js
@@ -2,17 +2,18 @@ import { combineReducers } from 'redux';
 import {DEFAULT_WORK_TIME, DEFAULT_BREAK_TIME, CHANGE_WORK_STATUS, RESET_WORK_STATUS, SET_CLOCK, STOP_CLOCK, START_PAUSE_CLOCK, UPDATE_TIMER, RESET_TIMER} from '../api/constants';
 import { Timer } from '../utils';
 
+
 //controls WORK and BREAK times
 const workStatusReducer = (state=true, action) => {
-    if(action.type === CHANGE_WORK_STATUS){
-        return !state;
-    }
 
-    if(action.type === RESET_WORK_STATUS){
-        return true;
+    switch(action.type){
+        case CHANGE_WORK_STATUS:
+            return !state;
+        case RESET_WORK_STATUS:
+            return true;
+        default: 
+        return state;
     }
-   
-    return state;
 }
 
 const initialValuesReducer = (state = {initialWorkTime: DEFAULT_WORK_TIME, initialBreakTime: DEFAULT_BREAK_TIME}, action) => {
@@ -21,25 +22,27 @@ const initialValuesReducer = (state = {initialWorkTime: DEFAULT_WORK_TIME, initi
 
 //The tic tac guy
 const clockReducer = (state = {timer:new Timer(), isRunning:false}, action) => {
-    if(action.type===SET_CLOCK){
-        state.timer.setDispatcher(action.payload.dispatch, action.payload.func) 
+    switch(action.type){
+        case SET_CLOCK:
+            state.timer.setDispatcher(action.payload.dispatch, action.payload.func) 
+            break;
+        case START_PAUSE_CLOCK:
+            !state.timer.isRunning?state.timer.start():state.timer.stop();
+            state.isRunning=state.timer.isRunning;
+            break;
+        case STOP_CLOCK:
+            state&&state.timer.stop();
+            state.isRunning=state.timer.isRunning; 
+            break;
+        default:
+            return state;
     }
-
-    if(action.type===START_PAUSE_CLOCK){
-        !state.timer.isRunning?state.timer.start():state.timer.stop();
-        state.isRunning=state.timer.isRunning;
-    }
-
-    if(action.type===STOP_CLOCK){
-        state&&state.timer.stop();
-        state.isRunning=state.timer.isRunning;
-    }
-
-    return state;
 }
 
 const timerReducer = (state = DEFAULT_WORK_TIME, action) => {
     switch(action.type){
+
+        //Decreases timer in one second
         case UPDATE_TIMER:
         return  state.sec>0?
                     state.min>=0?
@@ -49,6 +52,7 @@ const timerReducer = (state = DEFAULT_WORK_TIME, action) => {
                     ({min: state.min-1, sec: 59})
                     :state
 
+        //Resets timer values with initialValues set in initialValuesReducer
         case RESET_TIMER:
         return action.payload;
 
